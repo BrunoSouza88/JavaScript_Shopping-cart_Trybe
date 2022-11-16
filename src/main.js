29a8849996337050bbc90b665ebd21db536195fd
BrunoSouza <desouza88@hotmail.com>
2022-11-16 20:29:22 -0300

    Requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const elementUl = document.querySelector('.products');
+
+const createElementList = async () => {
+  const elementList = await fetchProductsList('computador');
+  elementList.forEach((element) => {
+    const list = createProductElement(element);
+    elementUl.appendChild(list);
+  });
+};
+
+window.onload = () => {
+  createElementList();
+};
