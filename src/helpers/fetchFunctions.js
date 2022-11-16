c2c1f499ae73b2d651f38c5a67dfca556f0d01b7
BrunoSouza <desouza88@hotmail.com>
2022-11-16 16:50:21 -0300

    Requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,10 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (QUERY) => {
+  if (!QUERY) throw new Error('Termo de busca não informado');
+
+  const UrlQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+  const result = await UrlQuery.json();
+  return result.results;
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,20 +1,32 @@
 import './mocks/fetchSimulator';
-import { fetchProductsList } from '../helpers/fetchFunctions';
+import { fetchProduct, fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function')
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled()
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   });
+
+  it(`Teste se o retorno da função fetchProductsList com o argumento 'computador' é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.`, () => {
+    fetchProductsList('computador');
+    expect(fetch).toEqual(computadorSearch)
+  });
+
+  it(`Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: 'Termo de busca não informado'.`, () => {
+    fetchProductsList();
+    expect(fetch).toThrow('Termo de busca não informado')
+  })
 
   // it('...', () => {
   // });
