6799ed9a15f672e335088e9cddaff90618181753
BrunoSouza <desouza88@hotmail.com>
2022-11-16 17:20:32 -0300

    Requisito 7

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,11 +1,15 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (productID) => {
+  if (!productID) throw new Error('ID não informado');
+
+  const urlProductID = await fetch(`https://api.mercadolibre.com/items/${productID}`);
+  const resultID = urlProductID.json();
+  return resultID;
 };
 
 export const fetchProductsList = async (QUERY) => {
   if (!QUERY) throw new Error('Termo de busca não informado');
 
-  const UrlQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
-  const result = await UrlQuery.json();
+  const urlQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+  const result = await urlQuery.json();
   return result.results;
 };
