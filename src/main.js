3d067e052712a0d4b5f467d175852bc24ce7f0cc
BrunoSouza <desouza88@hotmail.com>
2022-11-17 12:47:10 -0300

    Requisito 4

--- src/main.js
+++ src/main.js
@@ -7,8 +7,21 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const elementUl = document.querySelector('.products');
 
+const loadingScreen = () => {
+  const loading = document.createElement('p');
+  loading.innerText = 'carregando...';
+  loading.classList.add('loading');
+  elementUl.appendChild(loading);
+};
+
+const loadedScreen = () => {
+  document.querySelector('.loading').remove();
+};
+
 const createElementList = async () => {
+  loadingScreen();
   const elementList = await fetchProductsList('computador');
+  loadedScreen();
   elementList.forEach((element) => {
     const list = createProductElement(element);
     elementUl.appendChild(list);
